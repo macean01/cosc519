2e08b408b6db7e1ec606a5884598f57f5b70bc38
macean01 <macean01@gmail.com>
2013-12-03 20:55:58 -0500

    Revert "Project work"
    This reverts commit b9e1d18b2b8e73b7d030ddbd8ebb0344871b107e.

--- src/PatternType.java
+++
@@ -1,6 +0,0 @@
-
-public class PatternType {
-	public static final int Pair = 0;
-	public static final int Chain = 1;
-	public static final int Random = 2;
-}
--- src/Process.java
+++ src/Process.java
@@ -1,78 +1,74 @@
-public class Process extends Thread{
+public class Process {
+        
+        
+       //Notes:
+                //Process will need a variable, message_queue, that it can make calls to consume from and send messages to
+                //When constructing a message to be sent, how do we decide WHAT data goes into the message? will we just use random data?
 
-	// Notes:
-	// Process will need a variable, message_queue, that it can make calls to
-	// consume from and send messages to
-	// When constructing a message to be sent, how do we decide WHAT data goes
-	// into the message? will we just use random data?
+        
+        
+        
+        int pid; //pid set where? not here
+        int priority;//might be unnecessary
+        
+        char[] data; //received message data goes here? -- considering String type
+        
+        
+        
+        //constructor
+        public Process() {
+                pid = -1;
+                priority = 0;
+                data = new char[1024]; //arbitrary buffer..for now
 
-	int pid; // pid set where? not here
-	int priority;// might be unnecessary
-	String myQueueHandle;
-	String sendQueueHandle;
-	MessageQueueManager message_queue_manager;
-	char[] data; // received message data goes here? -- considering String type
-	Boolean isRandomSenario;
+        }
 
-	// constructor
-	public Process() {
-		pid = -1;
-		priority = 0;
-		data = new char[1024]; // arbitrary buffer..for now
-
-	}
-
-	public Process(int myPid, int myPriority,
-			int myBufferSize,MessageQueueManager manager) {
-		pid = myPid;
-		priority = myPriority;
-		data = new char[myBufferSize];
-		message_queue_manager = manager;
-	}
-
-	public boolean recMessage() {// this will call pop() on the Message Queue
-
-		// retrieve a message from the message queue
-		Message rec_message = message_queue_manager.read(myQueueHandle);
-		if(rec_message != null){
-			processMessage(rec_message); 
-		} else {
-			System.out.println(" [*] Message Queue Empty");
-			return false;
-		}
-		return true;
-		// there will need be some kind of controller for processes that waits
-		// to receive messages that runs this func
-
-	}
-
-	// copy over data from a message to a process' buffer
-	public void processMessage(Message msg) {
-		char[] data_to_get = msg.getMessage_data();
-		for (int i = 0; i < data_to_get.length; i++) {
-			data[i] = data_to_get[i];// copy over data to the process' buffer
-		}
-	}
-
-	public void sendMessageToQueue(char[] data, Process target) { // this will
-																	// call
-																	// write()
-																	// in the
-																	// MessageQueue
-																	// class
-		// send a message out to the message queue
-		Message send_message = new Message(data, target.pid, this.pid);
-
-		message_queue_manager.write(sendQueueHandle,send_message);// placeholder
-
-		// send message out to the queue
-		// queue should contain Message objects that contain a data portion and
-		// target (PID?)
-	}
-
-	
-	public void run(){
-		//need to implement behaviors here
-	}
-	
+        
+        
+        public boolean recMessage(){//this will call pop() on the Message Queue
+        
+                
+                //retrieve a message from the message queue
+                
+                if(message_queue.notEmpty()){
+                 Message rec_message = message_queue.consumeItem();//placeholder
+                 
+                 processMessage(rec_message); //store the recieved message in the buffer -- not sure if this is necessary
+                }
+                else{
+                        System.out.println(" [*] Message Queue Empty");
+                }
+                
+                //there will need be some kind of controller for processes that waits to receive messages that runs this func
+                
+        }
+        
+        //copy over data from a message to a process' buffer
+        public void processMessage(Message msg){
+                char[] data_to_get = msg.getMessage_data();
+                for(int i=0;i<data_to_get.length;i++){
+                        data[i] = data_to_get[i];//copy over data to the process' buffer
+                }
+        }
+        
+        public void sendMessageToQueue(char[] data, Process target){ //this will call write() in the MessageQueue class
+                        //send a message out to the message queue
+                        Message send_message = new Message(data, target.pid, this.pid);
+       
+                        
+                        message_queue.addItem(send_message);//placeholder
+                        
+                //send message out to the queue
+                //queue should contain Message objects that contain a data portion and target (PID?)
+        }
+ 
+        
+        
+        
+        
+        
+        
+        
+        
+        
 }
