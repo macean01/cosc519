930fcd08345fbe0da9e9e3c9a215c0ad838f7e29
andrewoconnor <andrewoconnor@outlook.com>
2013-12-09 17:53:11 -0500

    Fixed a race condition in MessageLock.

--- src/MessageLock.java
+++ src/MessageLock.java
@@ -5,7 +5,10 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class MessageLock {
 
+    private Thread owner = null;
     private volatile AtomicInteger numWritersWaiting = new AtomicInteger(0);
+    private volatile AtomicInteger numWritersWriting = new AtomicInteger(0);
+    private volatile AtomicInteger numReadersReading = new AtomicInteger(0);
     private final Lock lock = new ReentrantLock();
     private final Condition writersWriting = lock.newCondition();
     private final Condition readerReading = lock.newCondition();
@@ -15,11 +18,17 @@ public class MessageLock {
 
         lock.lock();
         try {
-            while (numWritersWaiting.get() > 0) {
+            while (numWritersWaiting.get() > 0 && numWritersWriting.get() > 0) {
                 synchronized (writersWriting) {
                     writersWriting.await();
                 }
             }
+
+            while (numReadersReading.get() > 0) {
+                readerReading.await();
+            }
+
+            numReadersReading.incrementAndGet();
         } catch (Exception e) { System.err.println(e); }
 
     }
@@ -27,29 +36,39 @@ public class MessageLock {
     public void readUnlock() {
 
         synchronized (readerReading) {
-            readerReading.signal();
+            readerReading.signalAll();
+            numReadersReading.decrementAndGet();
         }
         lock.unlock();
     }
 
     public void writeLock() throws InterruptedException {
 
-
         lock.lock();
         numWritersWaiting.incrementAndGet();
 
         try {
-            synchronized (readerReading) {
-                readerReading.await();
+            while (numReadersReading.get() > 0) {
+                synchronized (readerReading) {
+                    readerReading.await();
+                }
             }
+
+            while (numWritersWriting.get() > 0) {
+                writersWriting.await();
+            }
+
+            numWritersWaiting.decrementAndGet();
+            numWritersWriting.incrementAndGet();
+
         } catch (Exception e) { System.err.println(e); }
     }
 
     public void writeUnlock() {
 
         synchronized (writersWriting) {
-            numWritersWaiting.decrementAndGet();
-            writersWriting.signal();
+            numWritersWriting.decrementAndGet();
+            writersWriting.signalAll();
         }
         lock.unlock();
 
